bda1dd36f0bfe573de49c5243b844fdbef6bcd96
akayi07 <zhuolimiaotx@163.com>
2022-02-28 11:58:20 +0800

    modify directory of config.json

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
-cp ./config.json /etc/lib/v2ray/
+cp ./config.json /usr/local/etc/v2ray/
 sudo ufw disable
 sudo cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
 systemctl start v2ray
